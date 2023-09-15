3323845e9a03e88f4bdbc7458b4d380f60e98e93
Md Christien <mdchristien12@gmail.com>
2023-09-15 18:52:14 +0100

    Build the color mode switch

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,10 @@
+import ColorModeSwitch from "./components/ColorModeSwitch";
+
 function App() {
   return (
-    <>
-      <h1 className="my-2 container">Hello world</h1>
-      <div className="container">
-        <button type="button" className="btn btn-primary">
-          Primary
-        </button>
-        <button type="button" className="btn btn-secondary">
-          Secondary
-        </button>
-        <button type="button" className="btn btn-success">
-          Success
-        </button>
-        <button type="button" className="btn btn-danger">
-          Danger
-        </button>
-        <button type="button" className="btn btn-warning">
-          Warning
-        </button>
-        <button type="button" className="btn btn-info">
-          Info
-        </button>
-        <button type="button" className="btn btn-light">
-          Light
-        </button>
-        <button type="button" className="btn btn-dark">
-          Dark
-        </button>
-        <button type="button" className="btn btn-link">
-          Link
-        </button>
-      </div>
-    </>
+    <div>
+      <ColorModeSwitch />
+    </div>
   );
 }
 
