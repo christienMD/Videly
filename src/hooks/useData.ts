4ebbf067fd73fe994e1dce7f240120697ae39fa1
Md Christien <mdchristien12@gmail.com>
2023-10-23 21:27:02 +0100

    fixed the useData Hook's bug

--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -1,33 +1,33 @@
-import { useEffect, useState } from "react";
-// import { FetchMovieResponse } from "../services/movie-service";
-import movieService from "../services/movie-service";
-import axios from "axios";
+// import { useEffect, useState } from "react";
+// // import { FetchMovieResponse } from "../services/movie-service";
+// import movieService from "../services/movie-service";
+// import axios from "axios";
 
-const useData = <T>() => {
-  const [data, setData] = useState<T[]>([] || {} as T[]);
-  const [error, setError] = useState("");
-  const [isLoading, setLoading] = useState(false);
-  useEffect(() => {
-    setLoading(true);
-    const { request, cancle } = movieService.getAll<T>();
+// const useData = <T>() => {
+//   const [data, setData] = useState<T[]>([] || {} as T[]);
+//   const [error, setError] = useState("");
+//   const [isLoading, setLoading] = useState(false);
+//   useEffect(() => {
+//     setLoading(true);
+//     const { request, cancle } = movieService.getAll<T>();
 
-    request
-      .then((res) => {
-        setData(res.data);
-        setLoading(false);
-      })
-      .catch((err) => {
-        if (axios.isCancel(err)) {
-          return;
-        }
-        setError(err.message);
-        setLoading(false);
-      });
+//     request
+//       .then((res) => {
+//         setData(res.data);
+//         setLoading(false);
+//       })
+//       .catch((err) => {
+//         if (axios.isCancel(err)) {
+//           return;
+//         }
+//         setError(err.message);
+//         setLoading(false);
+//       });
 
-    return () => cancle();
-  }, []);
+//     return () => cancle();
+//   }, []);
 
-  return { data, setData, error, isLoading };
-};
+//   return { data, setData, error, isLoading };
+// };
 
-export default useData;
+// export default useData;
